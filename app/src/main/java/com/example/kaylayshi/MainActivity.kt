2d69bc17e0cc3741dbc932eabb5f7deb061fa5e6
kaylayshi <oparaiskelechi@gmail.com>
2022-12-15 00:49:21 +0100

    display kaylayshi card

--- app/src/main/java/com/example/kaylayshi/MainActivity.kt
+++ app/src/main/java/com/example/kaylayshi/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.kaylayshi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kaylayshi.ui.theme.KaylayshiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +33,9 @@ class MainActivity : ComponentActivity() {
             KaylayshiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateKaylayshiCard()
                 }
             }
         }
@@ -30,14 +43,142 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateKaylayshiCard() {
+    var buttonClickedState by remember { mutableStateOf(false) }
+    Surface(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(14.dp),
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            elevation = 4.dp
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider()
+                CreateInfo()
+
+                Button(onClick = { buttonClickedState = !buttonClickedState }) {
+                    Text(text = "Portfolio", style = MaterialTheme.typography.button)
+                }
+
+                if (buttonClickedState) {
+                    PortFolioContent()
+                } else {
+                    Box {}
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun PortFolioContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(4.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(4.dp),
+            shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+            border = BorderStroke(2.dp, Color.LightGray)
+        ) {
+            PortFolio(data = listOf("kele", "anth", "nkem", "zuri"))
+        }
+    }
+}
+
+@Composable
+fun PortFolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(14.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                        .padding(6.dp)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .align(CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great project", style = MaterialTheme.typography.body2)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateInfo() {
+    Column(
+        modifier = Modifier.padding(4.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Opara Kelechi",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+        Text(text = "Android Engineer", modifier = Modifier.padding(4.dp))
+        Text(text = "@riskymind", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(8.dp),
+        shape = CircleShape,
+        border = BorderStroke(1.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Profile image",
+            modifier = modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KaylayshiTheme {
-        Greeting("Android")
+        CreateKaylayshiCard()
     }
 }
